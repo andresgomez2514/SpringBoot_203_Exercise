a52012e035273b6ab7150370d300b7004b5c6ac1
GBTC408017UR <GBTC408017UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-14 10:36:41 -0400

    employee form

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String fname;
+    private String lname;
+    private String ssn;
+    private String dob;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
